f1bbe1e6773f8be1e56c4c20b0025f528ad7036a
lanzorg <anonymous@example.com>
2019-02-05 10:25:45 +0100

    Remove dummy comments

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,3 @@
-/* becodeorg/bookshelf
- *
- * /src/server/index.js - Server entry point
- *
- * coded by leny@BeCode
- * started at 18/01/2019
- */
-
 import express from "express";
 import path from "path";
 
