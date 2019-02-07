220784a4e7e84012b7657a551a0753e67996fe07
dagjo90 <da_gjo@hotmail.be>
2019-02-07 15:44:50 +0100

    login component ver:1.1

--- src/client/components/login.js
+++ src/client/components/login.js
@@ -3,25 +3,31 @@ import * as React from "react";
 export default class Login extends React.Component {
     render() {
         return (
-            <div className="login">
-                <h2>{" LOGIN "}</h2>
-                <form>
-                    <div className="input">
-                        <h3>{"e-mail"}</h3>
+            <div className="loginPage">
+                <div className="login">
+                    <h2>{" LOGIN "}</h2>
+                    <form>
+                        <div className="input">
+                            <h3>{"e-mail"}</h3>
+                            <input
+                                type="text"
+                                className="email"
+                                placeholder="email"
+                            />
+                            <h3>{"password"}</h3>
+                            <input
+                                type="password"
+                                className="password"
+                                placeholder="password"
+                            />
+                        </div>
                         <input
-                            type="text"
-                            className="email"
-                            placeholder="email"
+                            className="submit"
+                            type="submit"
+                            value="submit"
                         />
-                        <h3>{"password"}</h3>
-                        <input
-                            type="password"
-                            className="password"
-                            placeholder="password"
-                        />
-                    </div>
-                    <input className="submit" type="submit" value="submit" />
-                </form>
+                    </form>
+                </div>
             </div>
         );
     }
--- src/client/components/main.js
+++ src/client/components/main.js
@@ -1,12 +1,10 @@
 import * as React from "react";
-import Header from "./header";
 import Login from "./login";
 
 export default class Main extends React.Component {
     render() {
         return (
             <div className="main">
-                <Header />
                 <Login />
             </div>
         );
