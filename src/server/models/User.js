a8770abae6577c6645a96b0e1be455743802fdc9
lanzorg <anonymous@example.com>
2019-02-08 14:55:08 +0100

    Remove redundant code

--- src/server/models/User.js
+++ src/server/models/User.js
@@ -1,12 +1,5 @@
 import mongoose from "mongoose";
 
-mongoose
-    .connect("mongodb://dev:dev@mongo:27017/bebook?authSource=admin")
-    .then(() =>
-        console.log("Connection to MongoDB has been successfully established."),
-    )
-    .catch(err => console.log(err));
-
 const UserSchema = new mongoose.Schema(
     {
         firstName: {
