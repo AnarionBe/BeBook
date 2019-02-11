07134a83c1a39fc6c02a58051a04bd189dd4a7ef
dagjo90 <da_gjo@hotmail.be>
2019-02-11 10:14:19 +0100

    borrow extend reserve buttons

---
+++ src/client/components/ExtendButton.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default class ExtendButton extends React.Component {
+    render() {
+        return <button className="borrowButton">{"Extend"}</button>;
+    }
+}
---
+++ src/client/components/ReserveButton.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default class ReserveButton extends React.Component {
+    render() {
+        return <button className="borrowButton">{"Reserve"}</button>;
+    }
+}
