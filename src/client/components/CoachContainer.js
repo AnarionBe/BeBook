973bd8acb365491e0f6d12e47fcfbc3f81023d9d
rafacolsn <raphcolson@gmail.com>
2019-02-12 14:27:05 +0100

    table students

--- src/client/components/CoachContainer.js
+++ src/client/components/CoachContainer.js
@@ -1,49 +1,45 @@
 import * as React from "react";
-import Bookpane from "./Bookpane";
-import LateralMenu from "./LateralMenu";
-import img1 from "../assets/img/images.jpg";
-import img2 from "../assets/img/41PZk3+t3GL.jpg";
-import img3 from "../assets/img/51aIySRlwFL._SX402_BO1,204,203,200_.jpg";
-import img4 from "../assets/img/51cTWGKKlyL._SX260_.jpg";
-import img5 from "../assets/img/51FHuacxYjL._SX379_BO1,204,203,200_.jpg";
-import img6 from "../assets/img/51YHItqxXYL._SX413_BO1,204,203,200_.jpg";
-import img7 from "../assets/img/418+D1M5XTL._SX411_BO1,204,203,200_.jpg";
+import JuniorRow from "./JuniorRow";
 
 export default function CoachContainer() {
+    const data = [
+        {
+            Avatar: "../assets/avatar.jpg",
+            Firstname: "Raphaël",
+            Lastname: "Colson",
+        },
+        {
+            Avatar: "../assets/avatar.jpg",
+            Firstname: "John",
+            Lastname: "Doe",
+        },
+        {
+            Avatar: "../assets/avatar.jpg",
+            Firstname: "Ada",
+            Lastname: "Lovelace",
+        },
+        {
+            Avatar: "../assets/avatar.jpg",
+            Firstname: "Louis",
+            Lastname: "Labrocante",
+        },
+    ];
+
+    
+
+    const juniors = [];
+
+    for (let index = 0; index < data.length; index++) {
+        juniors.push(
+            <JuniorRow data={data[index]} />,
+        );
+    }
+
     return (
         <div className="container">
-            <Bookpane
-                title={
-                    "Design Patterns - Apprendre la conception de logiciels en réalisant un jeu vidéo (avec exercices et corrigés)"
-                }
-                className="bookpane"
-                isAvailable={true}
-                img={img1}
-            />
-            <Bookpane
-                title={"Apprendre à programmer avec Python 3"}
-                className="bookpane"
-                isAvailable={true}
-                img={img2}
-            />
-            <Bookpane
-                title={"Beginning HTML5 and CSS3 For Dummies"}
-                className="bookpane"
-                isAvailable={true}
-                img={img3}
-            />
-            <Bookpane
-                title={"Réalisez votre site web avec HTML 5 et CSS 3"}
-                className="bookpane"
-                isAvailable={true}
-                img={img4}
-            />
-            <Bookpane
-                title={"CSS 3 Flexbox: Plongez dans les CSS modernes."}
-                className="bookpane"
-                isAvailable={true}
-                img={img5}
-            />
+            <table>
+                <tbody>{juniors}</tbody>
+            </table>
         </div>
     );
 }
