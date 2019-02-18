0a77cf97aa68336b76ca65708e138225f0530a2b
dagjo90 <da_gjo@hotmail.be>
2019-02-18 12:15:10 +0100

    restoration

--- src/client/components/pages/Juniorpage/JuniorPage.js
+++ src/client/components/pages/Juniorpage/JuniorPage.js
@@ -1,16 +1,26 @@
-import * as React from "react";
+import React, {useState, useEffect} from "react";
 import JuniorContainer from "./JuniorContainer";
-import Header from "../../Header/Header";
-import LateralMenu from "../../LateralMenu/LateralMenu";
+import axios from "axios";
 
 export default function JuniorPage(props) {
-   
+    const [books, setBooks] = useState(undefined);
+
+    if (books === undefined) {
+        axios
+            .get(`/api/juniors/books`, {
+                headers: {
+                    authorization:
+                        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjNjNkNGRiNmFkMjI4MDA0YTYwMzAwNCIsImVtYWlsIjoianVuaW9yLmJhcnRAZ21haWwuY29tIiwiYXZhdGFyIjoiLy93d3cuZ3JhdmF0YXIuY29tL2F2YXRhci9lYjY2N2E2ODRlYzBlZGFjZWE4Y2U1Y2RlNzgwOWI0Mz9zPTIwMCZyPXBnJmQ9bW0iLCJpYXQiOjE1NTAxNDk0NTQsImV4cCI6MTU1MDc1NDI1NH0.83ovWRoydW4B6q0wFUEVmIq7FLishCXfL5Rtdnt_Chc",
+                },
+            })
+            .then(res => {
+                setBooks(res.data);
+            });
+    }
 
     return (
         <div className="main">
-            <Header switchLateralMenu={props.switchLateralMenu} isCoach={props.isCoach} />
-            <JuniorContainer />
-            <LateralMenu slide={props.slide} />
+            {books && <JuniorContainer books={books} />}
         </div>
     );
 }
